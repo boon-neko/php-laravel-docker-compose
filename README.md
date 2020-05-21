d43e142ede6cab37017c2903ab74464b612be523
boon-neko <amoru.tanaka@gmail.com>
2020-05-22 04:27:22 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,14 @@
+# bitekijyosi
+
+# docker起動
+$ docker-compose up -d
+
+# phpコンテナに入ります
+$ docker-compose exec php bash
+
+# Laravelプロジェクト作成
+$ laravel new
+
+
+# 参考
+https://qiita.com/A-Kira/items/1c55ef689c0f91420e81
---
+++ server/storage/framework/views/219dc15a98c78a3a2df3208ea03c10187e0f4c6d.php
@@ -0,0 +1,219 @@
+<?php require('header.blade.php'); ?>
+	<div class="key-visual">
+	  <ul class="slider" id="slider">
+	    <li><img src="static/img/slider-01.jpg" alt=""></li>
+	    <li><img src="static/img/slider-02.jpg" alt=""></li>
+	    <li><img src="static/img/slider-03.jpg" alt=""></li>
+	  </ul>
+	  <!-- <ul class="slider sp_sw" id="slider">
+	    <li><img src="static/img/slider-01-sp.png" alt=""></li>
+	    <li><img src="static/img/slider-02-sp.png" alt=""></li>
+	    <li><img src="static/img/slider-03-sp.png" alt=""></li>
+	  </ul> -->
+	</div>
+	<div class="top-ad-box">
+		<div class="container">
+			<div class="top-ad-inner">
+				<div class="ad-top-box">
+					<div class="ad-top-text">
+						<p><span class="ryumin">JR秋葉原駅徒歩5分、完全個室制のあなたの癒やし空間。<br class="pc_sw">新感覚エステサロン「美的女子。」へようこそ。ごゆっくりとご覧下さい。</span></p>
+					</div>
+					<div class="ad-top-btn">
+						<a href="https://lin.ee/IVgCCLRz"><img src="static/img/btn-img.png" alt=""></a>
+					</div>
+				</div>
+				<div class="bunner-box">
+					<ul>
+						<li><img src="static/img/bunner-img.jpg" alt="">
+						<li><img src="static/img/bunner-img.jpg" alt="">
+						<li><img src="static/img/bunner-img.jpg" alt="">
+					</ul>
+				</div>
+			</div>
+		</div>
+	</div>
+	<div class="sec-wrap">
+		<div class="container">
+			<div class="sec-head">
+				<div class="sec-head-title">
+					<p><span class="ryumin">NEWS・EVENT</span></p>
+				</div>
+				<div class="sec-head-jp">
+					<p><span class="ryumin">新着情報・イベント</span></p>
+				</div>
+			</div>
+			<div class="news-inner">
+				<ul>
+					<li>
+						<div class="">
+							<p><span class="news-tab ryumin">店舗情報</span>　<span class="ryumin news-day">2020.05.12</span></p>
+						</div>
+						<div class="news-label">
+							<a href="#">【新型コロナ感染症予防】衛生管理の取り組みについて</a>
+						</div>
+					</li>
+					<li>
+						<div class="">
+							<p><span class="news-tab ryumin">イベント</span>　<span class="ryumin news-day">2020.05.12</span></p>
+						</div>
+						<div class="news-label">
+							<a href="#">新コスチュームイベント開催！</a>
+						</div>
+					</li>
+					<li>
+						<div class="">
+							<p><span class="news-tab ryumin">キャンペーン</span>　<span class="ryumin news-day">2020.05.12</span></p>
+						</div>
+						<div class="news-label">
+							<a href="#">新人２０％OFFキャンペーン！</a>
+						</div>
+					</li>
+				</ul>
+			</div>
+		</div>
+	</div>
+	<div class="sec-wrap">
+		<div class="container">
+			<div class="sec-head">
+				<div class="sec-head-title">
+					<p><span class="ryumin">Today’s Schedule</span></p>
+				</div>
+				<div class="sec-head-jp">
+					<p><span class="ryumin">本日の出勤情報</span></p>
+				</div>
+			</div>
+			<div class="today-inner">
+				<div class="row">
+					<div class="box col-xs-6 col-sm-6 col-md-3 col-lg-3">
+						<div class="cast-box">
+							<a href="#">
+								<div class="cast-img">
+									<img src="static/img/cast-img01.png" alt="">
+								</div>
+								<div class="cast-desc">
+									<div class="cast-name">
+										<span class="ryumin">山田　花子</span>
+									</div>
+									<div class="cast-style">
+										<span class="ryumin">T.165 B.91(F)</span>
+									</div>
+									<div class="cast-word">
+										<p>超絶ハイクラス！グラマラス美女</p>
+									</div>
+									<div class="cast-schedule">
+										<p>21:30 - 05:00</p>
+									</div>
+								</div>
+							</a>
+						</div>
+					</div>
+					<div class="box col-xs-6 col-sm-6 col-md-3 col-lg-3">
+						<div class="cast-box">
+							<a href="#">
+								<div class="cast-img">
+									<img src="static/img/cast-img02.jpg" alt="">
+								</div>
+								<div class="cast-desc">
+									<div class="cast-name">
+										<span class="ryumin">成瀬　かな</span>
+									</div>
+									<div class="cast-style">
+										<span class="ryumin">T.165 B.91(F)</span>
+									</div>
+									<div class="cast-word">
+										<p>容姿端麗・スタイル抜群・極上美女</p>
+									</div>
+									<div class="cast-schedule">
+										<p>21:30 - 05:00</p>
+									</div>
+								</div>
+							</a>
+						</div>
+					</div>
+					<div class="box col-xs-6 col-sm-6 col-md-3 col-lg-3">
+						<div class="cast-box">
+							<a href="#">
+								<div class="cast-img">
+									<img src="static/img/cast-img03.jpg" alt="">
+								</div>
+								<div class="cast-desc">
+									<div class="cast-name">
+										<span class="ryumin">藤井 みゆ</span>
+									</div>
+									<div class="cast-style">
+										<span class="ryumin">T.165 B.91(F)</span>
+									</div>
+									<div class="cast-word">
+										<p>メンズエステ究極の慈愛施術</p>
+									</div>
+									<div class="cast-schedule">
+										<p>21:30 - 05:00</p>
+									</div>
+								</div>
+							</a>
+						</div>
+					</div>
+					<div class="box col-xs-6 col-sm-6 col-md-3 col-lg-3">
+						<div class="cast-box">
+							<a href="#">
+								<div class="cast-img">
+									<img src="static/img/cast-img04.jpg" alt="">
+								</div>
+								<div class="cast-desc">
+									<div class="cast-name">
+										<span class="ryumin">霧島 ゆき</span>
+									</div>
+									<div class="cast-style">
+										<span class="ryumin">T.165 B.91(F)</span>
+									</div>
+									<div class="cast-word">
+										<p>とっても綺麗でプロポーションも抜群</p>
+									</div>
+									<div class="cast-schedule">
+										<p>21:30 - 05:00</p>
+									</div>
+								</div>
+							</a>
+						</div>
+					</div>
+				</div>
+			</div>
+			<div class="ad-top-btn">
+				<a href="https://lin.ee/IVgCCLRz"><img src="static/img/btn-img.png" alt=""></a>
+			</div>
+		</div>
+	</div>
+	<div class="sec-wrap">
+		<div class="container">
+			<div class="sec-head">
+				<div class="sec-head-title">
+					<p><span class="ryumin">Twitter</span></p>
+				</div>
+				<div class="sec-head-jp">
+					<p><span class="ryumin">ツイッター</span></p>
+				</div>
+			</div>
+			<div class="twitter-inner">
+				<a class="twitter-timeline" data-height="600" data-theme="light" href="https://twitter.com/biteki_esthetic?ref_src=twsrc%5Etfw">Tweets by biteki_esthetic</a> <script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
+			</div>
+		</div>
+	</div>
+	<div class="sec-wrap">
+		<div class="container">
+			<div class="note-box">
+				<div class="note-box-top">
+					<img class="" src="static/img/logo-img-white.png" alt="">
+				</div>
+				<div class="note-inner">
+					<div class="note-inner-title">
+						<p><span class="ryumin">メンズエステサロン「美的女子。」は、<br class="sp_sw">風俗店ではございません。</span></p>
+					</div>
+					<div class="note-inner-text">
+						<p>非風俗を謳いながら、きわどい施術を強要する悪質店舗にご注意下さい。<br class="pc_sw">美的女子。ではセラピスト様へそういった、風俗のようなきわどい施術を要求しておりません。</p>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+	<?php require ('footer.php'); ?>
+<?php /**PATH /var/www/resources/views/index.blade.php ENDPATH**/ ?>
